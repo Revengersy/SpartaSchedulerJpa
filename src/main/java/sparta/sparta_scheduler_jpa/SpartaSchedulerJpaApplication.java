5ca36a055cf8c0e6b7611c8aeffe70de7e6d9c82
JunChoi <chldnjswns1@daum.net>
2024-12-18 16:54:10 +0900

    :tada:initial commit

---
+++ src/main/java/sparta/sparta_scheduler_jpa/SpartaSchedulerJpaApplication.java
@@ -0,0 +1,13 @@
+package sparta.sparta_scheduler_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaSchedulerJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaSchedulerJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sparta/sparta_scheduler_jpa/SpartaSchedulerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.sparta_scheduler_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaSchedulerJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
